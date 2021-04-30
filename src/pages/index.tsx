5e4c4e3c9363d79b2c6036d834a77892bac82064
Felipe Costa <felipehscbr@gmail.com>
2021-04-30 11:58:40 -0300

    added some configurations, episodes & static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
-import { Header } from  "../components/Header"
+import { useEffect } from  "react"
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props.episodes)
+ // useEffect(() => {
+ //   fetch('http://localhost:3333/episodes')
+ //       .then(response => response.json())
+ //       .then(data => console.log(data))
+ // }, [])
+
   return (
+      <div>
       <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+      </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+      revalidade: 60 * 60 * 8,
+  }
 }
