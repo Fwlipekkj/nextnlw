ec8aaaad51f3799a2e429105eb12639336139a1f
Felipe Costa <felipehscbr@gmail.com>
2021-04-30 09:16:54 -0300

    addes styles module & Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+ return (
+     <header>
+         <img src="/logo.svg" alt="Pocastr" />
+
+         <p>O melhor para você ouvir, sempre</p>
+
+         <span>Qui, 8 Abril</span>
+     </header>
+ )
+}
